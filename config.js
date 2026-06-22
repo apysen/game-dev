a52da5cea4d438cd2cf7dfb81cf9c7985ce3226b
apysen <apys502@gmail.com>
2026-06-21 23:24:48 -0700

    New difficulty

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ window.KG_CONFIG = {
   REPORT_EMAIL:   "apys502@gmail.com",
 
   PATCH_NOTES: [
+    { version: "0.2.6", date: "2026-06-21", notes: "• Added Grandmaster difficulty. Apologies in advance." },
     { version: "0.2.5", date: "2026-06-21", notes: "• Removed save name button so you can hit play as soon as you enter your name\n• Reworded the legend on the \"Was I trapped?\" snapshot to improve clarity" },
     { version: "0.2.4", date: "2026-06-21", notes: "Okay maybe I was wrong, there were, in fact, impossible arrangements in Trap mode, but that should hopefully be fixed now.\n\n• Modified procedural trap generation logic to hopefully never fully trap the player\n• \"Was I trapped?\" snapshot now shows your last 10 moves." },
     { version: "0.2.3", date: "2026-06-21", notes: "• Added patch notes\n• Added bug report" }
